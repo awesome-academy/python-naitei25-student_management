e6a9c4d42206d46a6799ca7b682beb8fdecd2165
“Kuwado” <viethoan557723@gmail.com>
2025-03-19 01:08:48 +0700

    Grade

--- student_management/school/views/action_views.py
+++ student_management/school/views/action_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.forms import formset_factory
-from school.models import Student, ClassStudent, Attendance
-from school.forms import AttendanceForm, AttendanceFormSet
+from school.models import Student, ClassStudent, Attendance, Grade
+from school.forms import AttendanceForm, GradeForm
 from django.utils.timezone import now
 
 
@@ -21,10 +21,6 @@ def post_attendance(request):
                 classstudent_id = form.cleaned_data["classstudent_id"]
                 status = form.cleaned_data["status"]
 
-                # Attendance.objects.filter(
-                #     classstudent__id=classstudent_id, date=now().date()
-                # ).update(status=status)
-
                 attendance, created = Attendance.objects.update_or_create(
                     classstudent__id=classstudent_id,
                     date=now().date(),
@@ -34,3 +30,40 @@ def post_attendance(request):
                 attendance.save()
 
     return redirect("attendance-tracking", class_id, semester_id)
+
+
+@login_required
+def post_grade(request):
+    class_id = request.session.get("class_id")
+    semester_id = request.session.get("semester_id")
+    subject_id = request.session.get("subject_id")
+
+    if request.method == "POST":
+        students = Student.objects.all()
+        GradeFormSetFactory = formset_factory(GradeForm)
+        formset = GradeFormSetFactory(request.POST)
+
+        if formset.is_valid():
+            for form in formset:
+                classstudent_id = form.cleaned_data["classstudent_id"]
+                oral_score = form.cleaned_data["oral_score"]
+                fifteen_min_score = form.cleaned_data["fifteen_min_score"]
+                one_period_score = form.cleaned_data["one_period_score"]
+                midterm_score = form.cleaned_data["midterm_score"]
+                final_score = form.cleaned_data["final_score"]
+
+                grade, created = Grade.objects.update_or_create(
+                    classstudent__id=classstudent_id,
+                    subject__id=subject_id,
+                    defaults={
+                        "oral_score": oral_score,
+                        "fifteen_min_score": fifteen_min_score,
+                        "one_period_score": one_period_score,
+                        "midterm_score": midterm_score,
+                        "final_score": final_score,
+                    },
+                )
+
+                grade.save()
+
+    return redirect("grade", class_id, semester_id, subject_id)
--- student_management/school/views/teacher_view.py
+++ student_management/school/views/teacher_view.py
@@ -6,11 +6,17 @@ from django.utils.timezone import now
 from django.db.models import Q
 from django.forms import formset_factory
 
-from school.models import Attendance
-
-
-from school.models import Class, Student, Subject, Semester, ClassTeacher, ClassStudent
-from school.forms import AttendanceForm, AttendanceFormSet
+from school.models import (
+    Class,
+    Student,
+    Subject,
+    Semester,
+    ClassTeacher,
+    ClassStudent,
+    Attendance,
+    Grade,
+)
+from school.forms import AttendanceForm, GradeForm
 
 
 class ClassListView(LoginRequiredMixin, ListView):
@@ -207,5 +213,85 @@ class AttendancesView(LoginRequiredMixin, ListView):
         context["is_homeroom"] = self.is_homeroom
         context["formset"] = formset
         context["form_attendance_pairs"] = form_attendance_pairs
+
+        return context
+
+
+class GradeView(LoginRequiredMixin, ListView):
+    model = Grade
+    template_name = "school/grades/grade_list.html"
+    context_object_name = "grades"
+
+    def dispatch(self, request, *args, **kwargs):
+        if "class_id" in kwargs:
+            request.session["class_id"] = kwargs["class_id"]
+
+        if "semester_id" in kwargs:
+            request.session["semester_id"] = kwargs["semester_id"]
+
+        if "subject_id" in kwargs:
+            request.session["subject_id"] = kwargs["subject_id"]
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        class_id = self.kwargs.get("class_id")
+        semester_id = self.kwargs.get("semester_id")
+        subject_id = self.kwargs.get("subject_id")
+        class_teacher = ClassTeacher.objects.filter(
+            classroom__id=class_id,
+            semester__id=semester_id,
+            teacher=self.request.user,
+        ).get()
+        self.is_homeroom = class_teacher.is_homeroom
+
+        class_students = ClassStudent.objects.filter(
+            classroom__id=class_id, semester__id=semester_id
+        )
+
+        query = self.request.GET.get("student_name", "")
+        if query:
+            class_students = class_students.filter(
+                Q(student__first_name__icontains=query)
+                | Q(student__last_name__icontains=query)
+            )
+
+        grade_ids = []
+        for class_student in class_students:
+            grade, created = Grade.objects.get_or_create(
+                classstudent=class_student, subject__id=subject_id
+            )
+            grade_ids.append(grade.id)
+
+        return Grade.objects.filter(id__in=grade_ids)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        grades = self.get_queryset()
+
+        GradeFormSet = formset_factory(GradeForm, extra=0)
+        formset = GradeFormSet(
+            initial=[
+                {
+                    "classstudent_id": a.classstudent.id,
+                    "oral_score": a.oral_score,
+                    "fifteen_min_score": a.fifteen_min_score,
+                    "one_period_score": a.one_period_score,
+                    "midterm_score": a.midterm_score,
+                    "final_score": a.final_score,
+                }
+                for a in grades
+            ]
+        )
+
+        form_grade_pairs = zip(formset.forms, grades)
+
+        context["current_date"] = now().date()
+        context["class_id"] = self.kwargs.get("class_id")
+        context["semester_id"] = self.kwargs.get("semester_id")
+        context["subject_id"] = self.kwargs.get("subject_id")
+        context["is_homeroom"] = self.is_homeroom
+        context["formset"] = formset
+        context["form_grade_pairs"] = form_grade_pairs
 
         return context
